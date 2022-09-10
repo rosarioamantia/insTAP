46971f19cc4027f8f3d519c176eef6c8411503e6
Gianluca Di Franco <gianluk30@gmail.com>
2022-09-10 20:30:36 +0200

    WiP prod-logstash-kafka

--- producer/instap-producer.py
+++ producer/instap-producer.py
@@ -1,25 +1,20 @@
 import os
 from json import dumps
 from time import sleep
+import requests
 
 import instaloader
-from kafka import KafkaProducer
 
-TOPIC = os.getenv("KAFKA_TOPIC", "instap")
 POSTS_LIMIT = os.getenv( "POSTS_LIMIT", 10)
 COMMENTS_LIMIT = os.getenv("COMMENTS_LIMIT", 10)
 USER_TEST = os.getenv("USER_TEST","<user_here>")
 PASS_TEST = os.getenv("PASS_TEST","<pass_here>")
 USER_TO_WATCH = os.getenv("USER_TO_WATCH", "chiaraferragni")
+LOGSTASH_URL = "http://logstash:9600"
+PROJEJCT_ID = 'instap_id'
 
-
-producer = KafkaProducer(bootstrap_servers=['broker:29092'],
-                         value_serializer=lambda x:
-                         dumps(x).encode('utf-8'),
-                        api_version=(0, 10, 1))
 insta = instaloader.Instaloader()
 insta.login(USER_TEST, PASS_TEST)
-
 
 posts = instaloader.Profile.from_username(insta.context, USER_TO_WATCH).get_posts()
 for i, post in enumerate(posts):
@@ -31,5 +26,5 @@ for i, post in enumerate(posts):
             break
         data = {'user': USER_TO_WATCH, 'comment': comment.text}
         print(str(data))
-        producer.send(TOPIC, value=data)
+        x = requests.post(LOGSTASH_URL, json = data, timeout=2.50)
         sleep(5)
