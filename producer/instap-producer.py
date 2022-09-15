b61c649e43ee86dc339a980a93362fdaa1fa1e48
Gianluca Di Franco <gianluk30@gmail.com>
2022-09-15 22:26:19 +0200

    Producer sends post with 50 comment, image and caption.

--- producer/instap-producer.py
+++ producer/instap-producer.py
@@ -5,10 +5,10 @@ import requests
 
 import instaloader
 
-POSTS_LIMIT = os.getenv( "POSTS_LIMIT", 10)
-COMMENTS_LIMIT = os.getenv("COMMENTS_LIMIT", 10)
-USER_TEST = os.getenv("USER_TEST","<user_here>")
-PASS_TEST = os.getenv("PASS_TEST","<pass_here>")
+POSTS_LIMIT = os.getenv("POSTS_LIMIT", 50)
+COMMENTS_LIMIT = os.getenv("COMMENTS_LIMIT", 50)
+USER_TEST = os.getenv("USER_TEST", "<user_here>")
+PASS_TEST = os.getenv("PASS_TEST", "<pass_here>")
 USER_TO_WATCH = os.getenv("USER_TO_WATCH", "chiaraferragni")
 LOGSTASH_URL = "http://logstash:9700"
 PROJEJCT_ID = 'instap_id'
@@ -20,11 +20,21 @@ posts = instaloader.Profile.from_username(insta.context, USER_TO_WATCH).get_post
 for i, post in enumerate(posts):
     if i == POSTS_LIMIT:
         break
+    post_caption = post.caption
+    post_image_url = post.url
     comments = post.get_comments()
+    comments_to_send = []
     for index, comment in enumerate(comments):
         if index == COMMENTS_LIMIT:
             break
-        data = {'user': USER_TO_WATCH, 'comment': comment.text}
-        print(str(data))
-        x = requests.post(LOGSTASH_URL, json = data, timeout=2.50)
-        sleep(5)
+        comments_to_send.append(comment.text)
+
+    data = {
+        'id': i,
+        'user': USER_TO_WATCH,
+        'comments': ", ".join(comments_to_send),
+        'caption': post_caption,
+        'pic_url': post_image_url
+    }
+    print(str(data))
+    x = requests.post(LOGSTASH_URL, json=data, timeout=5)
