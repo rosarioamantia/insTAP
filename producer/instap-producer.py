689506cd64ea9632e79fbdc2670452cb16668c2e
Gianluca Di Franco <gianluk30@gmail.com>
2022-09-28 21:59:17 +0200

    WiP docker-compose

--- producer/instap-producer.py
+++ producer/instap-producer.py
@@ -1,4 +1,4 @@
-import os
+'''import os
 from json import dumps
 from time import sleep
 import requests
@@ -37,4 +37,32 @@ for i, post in enumerate(posts):
         'pic_url': post_image_url
     }
     print(str(data))
-    x = requests.post(LOGSTASH_URL, json=data, timeout=5)
+    x = requests.post(LOGSTASH_URL, json=data, timeout=5)'''
+
+import os
+from json import dumps
+from time import sleep
+
+import instaloader
+from kafka import KafkaProducer
+
+TOPIC = os.getenv("KAFKA_TOPIC", "instap")
+#POSTS_LIMIT = os.getenv( "POSTS_LIMIT", 10)
+#COMMENTS_LIMIT = os.getenv("COMMENTS_LIMIT", 10)
+#USER_TEST = os.getenv("USER_TEST","sentimentoanalisi")
+#PASS_TEST = os.getenv("PASS_TEST","Ciao12345")
+#USER_TO_WATCH = os.getenv("USER_TO_WATCH", "chiaraferragni")
+
+
+producer = KafkaProducer(bootstrap_servers=['broker:9092'],
+                         value_serializer=lambda x:
+                         dumps(x).encode('utf-8'))
+#insta = instaloader.Instaloader()
+#insta.login(USER_TEST, PASS_TEST)
+
+i = 0
+while i < 1000000000:
+    data = {'user': "ciao", 'id': i}
+    producer.send(TOPIC, value=data)
+    sleep(5)
+    i+=1
